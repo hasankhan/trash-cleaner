dc42e2a518257c6ceacc88380e84f8252d3b7b0d
Muhammad Hasan Khan <slashdot10@gmail.com>
2021-07-01 09:01:35 -0700

    Add tests for ConfigStore

---
+++ test/config-store.spec.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+const tmp = require('tmp');
+
+const { assert } = require('chai');
+const { FileSystemConfigStore } = require("../lib/config-store");
+
+const FILE_TEST = 'test.json';
+describe('FileSystemConfigStore', () => {
+    var configDirPath, cleanupDir;
+
+    before((done) => {
+        tmp.dir((err, path, cleanup) => {
+            if (err) throw err;
+
+            configDirPath = path;
+            cleanupDir = cleanup;
+            done();
+        });
+    });
+
+    after(() => {
+        if (cleanupDir) {
+            cleanupDir();
+        }
+    });
+
+    describe('get', () => {
+        it('parses value as json', async () => {
+            var store = new FileSystemConfigStore(configDirPath);
+            fs.writeFileSync(path.join(configDirPath, FILE_TEST), '[3]');
+
+            let value = await store.get(FILE_TEST);
+
+            assert.deepEqual(value, [3]);
+        });
+
+        it('returns null when key does not exist', async () => {
+            var store = new FileSystemConfigStore(configDirPath);
+
+            let value = await store.get(FILE_TEST + '.old');
+
+            assert.isNull(value);
+        });
+    });
+
+    describe('put', () => {
+        it('saves value as json', async () => {
+            var store = new FileSystemConfigStore(configDirPath);
+            await store.put(FILE_TEST, { val: 3 });
+
+            let value = fs.readFileSync(path.join(configDirPath, FILE_TEST), 'utf-8');
+
+            assert.equal(value, '{"val":3}');
+        });
+    });
+});
